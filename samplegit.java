adae47f52c08d6cdb6ce470908f0f070aac51b4b
Sivakalyan1234 <166099786+Sivakalyan1234@users.noreply.github.com>
2024-08-01 13:38:07 +0530

    Add files via upload
    create java file

---
+++ samplegit.java
@@ -0,0 +1,14 @@
+package samplegitproject;
+
+public class samplegit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		String s="first";
+		System.out.println(i);
+		System.out.println(s);
+
+	}
+
+}
